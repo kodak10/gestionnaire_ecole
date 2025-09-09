7f243cc73260e520b3b36896556208f85f4dad26
Kodak10 <atchinaymard10@gmail.com>
2025-09-09 13:06:00 +0000

    ajout d'une seul ecole et d'un seul user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,17 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Créer l'école si elle n'existe pas
-        $ecole = Ecole::firstOrCreate(
-            ['nom' => 'EPP Excelle'],
-            [
-                'adresse' => '123 Rue de l\'École, Abidjan',
-                'telephone' => '+225 01 23 45 67',
-                'email' => 'contact@eppexcelle.ci',
-                'directeur' => 'M. Admin',
-                'logo' => null // ou mettre un chemin vers le logo par défaut
-            ]
-        );
+        
 
         // Créer l'année scolaire si elle n'existe pas
         $anneeScolaire = AnneeScolaire::firstOrCreate(
