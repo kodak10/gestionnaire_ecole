f732a952b6b674741751530e4f07c67479abe1e3
Kodak10 <atchinaymard10@gmail.com>
2025-09-14 23:31:42 +0000

    add de ecole_id dans notes

--- database/migrations/2025_09_14_233033_make_annee_scolaire_nullable_on_notes_table.php
+++ database/migrations/2025_09_14_233033_make_annee_scolaire_nullable_on_notes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('notes', function (Blueprint $table) {
-            $table->dropColumn('annee_scolaire');
+            //
         });
     }
 
