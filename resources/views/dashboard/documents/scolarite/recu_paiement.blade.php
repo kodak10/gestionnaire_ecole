9dffd5f5626575ea2f5fac9005413f6f78197591
Kodak10 <atchinaymard10@gmail.com>
2025-09-15 06:56:14 +0000

    fix deux recu par page

--- resources/views/dashboard/documents/scolarite/recu_paiement.blade.php
+++ resources/views/dashboard/documents/scolarite/recu_paiement.blade.php
@@ -104,7 +104,8 @@
 </style>
 </head>
 <body>
-  <div class="receipt">
+    <div class="receipt">
+      <div class="receipt">
     <div class="header">
       <!-- Logo à gauche -->
       <img src="{{ public_path('assets/img/logo_excelle.jpg') }}" alt="Logo école" style="height:60px; vertical-align:middle; margin-right:10px;">
@@ -209,5 +210,116 @@
       
     </div>
   </div>
+
+<div style="border-top:1px dashed #000; margin:20px 0;"></div>
+
+  <div class="receipt">
+    <div class="receipt">
+    <div class="header">
+      <!-- Logo à gauche -->
+      <img src="{{ public_path('assets/img/logo_excelle.jpg') }}" alt="Logo école" style="height:60px; vertical-align:middle; margin-right:10px;">
+
+      <!-- Nom et téléphone -->
+      <h1 style="display:inline-block; margin:0; font-size:18px; vertical-align:middle;">
+          {{ $ecole->nom ?? 'GS EXCELLE' }}
+      </h1>
+      <span style="display:block; font-size:11px; margin-top:2px;">
+          {{ $ecole->telephone ?? '0708395524 / 0708395524' }}
+      </span>
+
+      <!-- Infos du reçu (numéro + date) -->
+      <div class="meta">
+          N°: {{ str_pad($paiement->id, 6, '0', STR_PAD_LEFT) }}<br>
+          Date: {{ date('d/m/Y H:i', strtotime($paiement->created_at)) }}
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="field">
+        <span class="label">Type de Reçu :</span>
+        <span class="value">
+          <strong>
+          @php
+            $typesFrais = [];
+            foreach($paiement->details as $detail) {
+              if($detail->typeFrais) {
+                $typesFrais[] = $detail->typeFrais->nom;
+              }
+            }
+            echo implode(', ', array_unique($typesFrais));
+          @endphp
+          </strong>
+        </span>
+      </div>
+
+      <div class="field">
+        <span class="label">Classe :</span>
+        <span class="value">{{ $classe->nom ?? '' }}</span>
+      </div>
+
+      <!-- Matricule + Nom et prénoms -->
+      <table class="two-cols">
+        <tr>
+          <td>
+            <span class="label">Matricule :</span>
+            <span class="value">{{ $eleve->matricule ?? '' }}</span>
+          </td>
+          <td>
+            <span class="label">Nom & Prénoms :</span>
+            <span class="value">{{ $eleve->nom ?? '' }} {{ $eleve->prenom ?? '' }}</span>
+          </td>
+        </tr>
+      </table>
+
+      <div class="field">
+        <span class="label">Libellé :</span>
+        <span class="value">
+          <strong>
+          @php
+            $typesFrais = [];
+            foreach($paiement->details as $detail) {
+              if($detail->typeFrais) {
+                $typesFrais[] = $detail->typeFrais->nom;
+              }
+            }
+            echo implode(', ', array_unique($typesFrais));
+          @endphp
+          </strong>
+        </span>
+      </div>
+
+      <!-- Montant versé + Reste à payer -->
+      <table class="two-cols">
+        <tr>
+          <td>
+            <span class="label">Montant versé :</span>
+            <span class="value">{{ number_format($montant_total, 0, ',', ' ') }} FCFA</span>
+          </td>
+          <td>
+            <span class="label">Reste à payer :</span>
+            <span class="value">{{ number_format($reste_total, 0, ',', ' ') }} FCFA</span>
+          </td>
+        </tr>
+      </table>
+
+      <!-- Mode paiement + Encaissé par -->
+      <table class="two-cols">
+        <tr>
+          <td>
+            <span class="label">Mode paiement :</span>
+            <span class="value">{{ $paiement->mode_paiement ?? 'Non spécifié' }}</span>
+          </td>
+          <td>
+            <span class="label">Encaissé par :</span>
+            <span class="value">{{ $paiement->user->name ?? '' }}</span>
+          </td>
+        </tr>
+      </table>
+
+      
+    </div>
+  </div>
+  </div>
+    </div>
 </body>
 </html>
